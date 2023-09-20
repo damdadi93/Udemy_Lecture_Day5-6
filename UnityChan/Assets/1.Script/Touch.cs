975a080787880107373f9c86f13a8f3c61634321
damdadi93 <77727581+damdadi93@users.noreply.github.com>
2023-09-20 15:55:58 +0900

    touch animtor
    1.터치한 대상에 따라 애니메이션과 소리가 다름
    2.애니메이터 시트에 접근이 가능하다.!!!!
    3.접근해서 얻은 정보로 애니매이션을 원하는데로 설정할수 있다.

--- UnityChan/Assets/1.Script/Touch.cs
+++ UnityChan/Assets/1.Script/Touch.cs
@@ -4,24 +4,74 @@ using UnityEngine;
 
 public class Touch : MonoBehaviour
 {
+    public AudioClip voice1;
+    public AudioClip voice2;
+    Animator animator;
+    AudioSource univoice;
+
+    //모션 스테이트의 ID 얻기
+    int motionIdol = Animator.StringToHash("Base Layer.Idol");
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();    
+        univoice = GetComponent<AudioSource>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
+        animator.SetBool("Touch", false);
+        animator.SetBool("TouchHead", false);
+
+        //재생 중인 동작이 대기 동작인지 검사
+        if (animator.GetCurrentAnimatorStateInfo(0).fullPathHash == motionIdol)
+        {
+            animator.SetBool("Motion_Idle", true);
+        }
+        else
+            animator.SetBool("Motion_Idle", false);
+
         if(Input.GetMouseButtonDown(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
+
             Debug.DrawRay(ray.origin, ray.direction * 1000, Color.red);
-            if (Physics.Raycast(ray, out hit, 100))
-            {
-                Debug.Log("hit!");
+
+            //if (Physics.Raycast(ray, out hit, 100))
+            //{
+            //    GameObject hitObj = hit.collider.gameObject;
+
+            //    Debug.Log("hit!");
+            //    animator.SetBool("Touch", true);
+            //    univoice.clip = voice2;
+            //    univoice.Play();
                 
+            //}
+            if(Physics.Raycast(ray,out hit,100))
+            {
+                GameObject hitObj = hit.collider.gameObject;
+                if (hitObj.tag == "Head")
+                {
+                    animator.SetBool("TouchHead", true);
+                    animator.SetBool("Face_Happy", true);
+                    animator.SetBool("Face_Angry", false);
+                    univoice.clip = voice1;
+                    univoice.Play();
+                }
+                else if (hitObj.tag == "Body")
+                {
+                    animator.SetBool("Touch", true);
+                    animator.SetBool("Face_Happy", false);
+                    animator.SetBool("Face_Angry", true);
+                    univoice.clip = voice2;
+                    univoice.Play();
+                }
+               
+                    
             }
         }
     }
